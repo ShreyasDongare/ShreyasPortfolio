4b5d3e206c287388063233085c60a6c0ea6b8eb3
ShreyasDongare <dongareshreyas@gmail.com>
2022-11-24 14:54:46 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import HeroImage from "../assets/HeroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div
+        className="max-w-screen-lg  mx-auto flex flex-col items-center
+      justify-center h-full md:flex-row text-white"
+      >
+        <div className="flex flex-col justify-center h-full mt-6 mx-5">
+          <h2 className="text-1xl sm:text-3xl">Hello, My name is</h2>
+          <h2 className="text-3xl sm:text-5xl">Shreyas Dongare</h2>
+          <h2 className=" text-4xl sm:my-3 ">I'm a <span className="text-rose-600">React Developer</span></h2>
+          <p className="sm:my-2">
+            I have total 3 years of experiance production and sales. Currently,
+            I love to work on building and desigining Single Page Web
+            Application using technology like ReactJs, HTML5, CSS, Tailwind,
+            Bootstrap.
+          </p>
+          <div>
+            <button className="group flex justify-center items-center cursor-pointer  bg-gradient-to-l from-sky-700 to-teal-600 rounded-md px-5 py-3">
+              Portfolio
+              <span className="mx-1 group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="ShreyasImage"
+            className=" mx-auto  rounded-2xl w-2/3   "
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+const [nav, setNav]= useState(false)
+
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "portfolio",
+    },
+    {
+      id: 4,
+      link: "experiance",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+
+  return (
+    <div className="flex justify-between items-center px-4 w-full h-20 text-white fixed bg-black">
+      <div className="z-10 select-none">
+        <h1 className="text-5xl font-signature ml-2 z-10">Shreyas</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => {
+          return (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize text-gray-500 font-medium hover:scale-105 duration-200"
+            >
+              {link}
+            </li>
+          );
+        })}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer text-gray-400 z-10  mr-4  md:hidden"
+      >
+        {nav ? (
+            <FaTimes
+              className="transition-all duration-300 hover:rotate-180"
+              size={30}
+            />
+            ) : (
+                <FaBars
+                  className="transition-all duration-300 hover:rotate-180"
+                  size={30}
+                />
+                )}
+      </div>
+      {nav && (
+        <ul className=" flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-700 text-gray-500 md:hidden">
+          {links.map(({ id, link }) => {
+            return (
+              <li
+                key={id}
+                className="px-4 cursor-pointer capitalize text-4xl py-5 my-1"
+              >
+                {link}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
